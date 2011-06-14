994d0471d2ec9b997dc598778412c7ddba133b6c
Wyatt Neal <wyatt.neal+git@gmail.com>
2011-06-13 21:55:26 -0400

    updating to include new getopt

--- rrsync.rb
+++ rrsync.rb
@@ -1,26 +1,38 @@
 #!/usr/bin/ruby
 require 'rubygems'
-require 'Logger'
+require 'logger'
 require 'benchmark'
 require 'ping'
-require 'FileUtils'
 require 'open3'
+require 'Getopt/Declare'
+
+include Getopt
 
 #============================= OPTIONS ==============================#
 # == Options for local machine.
 SSH_APP       = 'ssh'
 RSYNC_APP     = 'rsync'
 
-EXCLUDE_FILE  = '/path/to/.rsyncignore'
-DIR_TO_BACKUP = '/folder/to/backup'
-LOG_FILE      = '/var/log/rrsync.log'
+options = Getopt::Declare.new(<<EOF)
+  -e <file>     file of exclusions  [optional]
+  -r <rdir>     remote directory to backup [required]
+  -b <bdir>     local backup directory repository [required]
+  -l <file>     log file location [optional]
+  -a <age>      age of the backups to keep around [required]
+  -t <host>     remote host to pull backup from [required]
+  -p <port>     port for ssh  [optional]
+EOF
+  
+EXCLUDE_FILE  = '~/.rsyncignore' unless options["e"]
+DIR_TO_BACKUP = options["r"]
+LOG_FILE      = options["l"]
 LOG_AGE       = 'daily'
 
 EMPTY_DIR     = '/tmp/empty_rsync_dir/' #NEEDS TRAILING SLASH.
 # == Options for the remote machine.
-SSH_USER      = 'USER'
-SSH_SERVER    = 'HOSTNAME or IP'
-SSH_PORT      = '' #Leave blank for default (port 22).
+# SSH_USER      = 'USER' # we're going to run under the cron user
+SSH_SERVER    = options["t"]
+SSH_PORT      = '' unless options["p"]
 BACKUP_ROOT   = '/path/on/remote/machine/to/backup/folder'
 BACKUP_DIR    = BACKUP_ROOT + '/' + Time.now.strftime('%A').downcase
 RSYNC_VERBOSE = '-v'
@@ -38,15 +50,15 @@ else
   logger = Logger.new(nil)
 end
 ssh_port = SSH_PORT.empty? ? '' : "-e 'ssh -p #{SSH_PORT}'"
-rsync_cleanout_cmd = "#{RSYNC_APP} #{RSYNC_VERBOSE} #{ssh_port} --delete -a #{EMPTY_DIR} #{SSH_USER}@#{SSH_SERVER}:#{BACKUP_DIR}"
-rsync_cmd = "#{RSYNC_APP} #{RSYNC_VERBOSE} #{ssh_port} #{RSYNC_OPTS} #{DIR_TO_BACKUP} #{SSH_USER}@#{SSH_SERVER}:#{BACKUP_ROOT}/current"
+rsync_cleanout_cmd = "#{RSYNC_APP} #{RSYNC_VERBOSE} #{ssh_port} --delete -a #{EMPTY_DIR} #{SSH_SERVER}:#{BACKUP_DIR}"
+rsync_cmd = "#{RSYNC_APP} #{RSYNC_VERBOSE} #{ssh_port} #{RSYNC_OPTS} #{DIR_TO_BACKUP} #{SSH_SERVER}:#{BACKUP_ROOT}/current"
 
 logger.info("Started running at: #{Time.now}")
 run_time = Benchmark.realtime do
   begin
     raise Exception, "Unable to find remote host (#{SSH_SERVER})" unless Ping.pingecho(SSH_SERVER)
        
-    FileUtils.mkdir_p("#{EMPTY_DIR}")
+    Dir.mkdir("#{EMPTY_DIR}")
     Open3::popen3("#{rsync_cleanout_cmd}") { |stdin, stdout, stderr|
       tmp_stdout = stdout.read.strip
       tmp_stderr = stderr.read.strip

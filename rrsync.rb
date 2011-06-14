f5ace4117090d31a7ea16a50f0fdb16b0d3b0757
Wyatt Neal <wyatt.neal+git@gmail.com>
2011-06-13 23:02:08 -0400

    before reversing of script

--- rrsync.rb
+++ rrsync.rb
@@ -20,57 +20,61 @@ options = Getopt::Declare.new(<<EOF)
   -l <file>     log file location [optional]
   -a <age>      age of the backups to keep around [required]
   -t <host>     remote host to pull backup from [required]
+  -u <user>     remote host user that we log in as [required]
   -p <port>     port for ssh  [optional]
 EOF
   
-EXCLUDE_FILE  = '~/.rsyncignore' unless options["e"]
-DIR_TO_BACKUP = options["r"]
-LOG_FILE      = options["l"]
-LOG_AGE       = 'daily'
+exclude_file  = '~/.rsyncignore' unless options["-e"]
+remote_dir = options["-r"]
+log_file      = options["-l"]
+log_age       = 'daily'
 
-EMPTY_DIR     = '/tmp/empty_rsync_dir/' #NEEDS TRAILING SLASH.
+EMPTY_DIR     = '/tmp/empty_rsync_dir/' #NEEDS TRAILING SLASH. TODO: replace this with a tmp dir made randomly each time
 # == Options for the remote machine.
-# SSH_USER      = 'USER' # we're going to run under the cron user
-SSH_SERVER    = options["t"]
-SSH_PORT      = '' unless options["p"]
-BACKUP_ROOT   = '/path/on/remote/machine/to/backup/folder'
-BACKUP_DIR    = BACKUP_ROOT + '/' + Time.now.strftime('%A').downcase
-RSYNC_VERBOSE = '-v'
-RSYNC_OPTS    = "--force --ignore-errors --delete-excluded --exclude-from=#{EXCLUDE_FILE} --delete --backup --backup-dir=#{BACKUP_DIR} -a"
+remote_backup_target    = options["-t"]
+remote_user             = options["-u"]
+ssh_port                = '' unless options["-p"]
+backup_dir              = options["-b"] + '/' + Time.now.strftime('%Y%m%d')
+
+RSYNC_VERBOSE           = '-v'
+RSYNC_OPTS = "--force --ignore-errors --delete-excluded --exclude-from=#{exclude_file} --delete --backup --backup-dir=#{backup_dir} -a --bwlimit=200"
+
 # == Options to control output
 DEBUG         = true #If true output to screen else output is sent to log file.
 SILENT        = false #Total silent = no log or screen output.
 #========================== END OF OPTIONS ==========================#
 
 if DEBUG && !SILENT
-  logger = Logger.new(STDOUT, LOG_AGE)
-elsif LOG_FILE != '' && !SILENT
-  logger = Logger.new(LOG_FILE, LOG_AGE)
+  logger = Logger.new(STDOUT, log_age)
+elsif log_file != '' && !SILENT
+  logger = Logger.new(log_file, log_age)
 else
   logger = Logger.new(nil)
 end
-ssh_port = SSH_PORT.empty? ? '' : "-e 'ssh -p #{SSH_PORT}'"
-rsync_cleanout_cmd = "#{RSYNC_APP} #{RSYNC_VERBOSE} #{ssh_port} --delete -a #{EMPTY_DIR} #{SSH_SERVER}:#{BACKUP_DIR}"
-rsync_cmd = "#{RSYNC_APP} #{RSYNC_VERBOSE} #{ssh_port} #{RSYNC_OPTS} #{DIR_TO_BACKUP} #{SSH_SERVER}:#{BACKUP_ROOT}/current"
+ssh_port = ssh_port.empty? ? '' : "-e 'ssh -p #{ssh_port}'"
+rsync_cleanout_cmd = "#{RSYNC_APP} #{RSYNC_VERBOSE} #{ssh_port} --delete -a #{EMPTY_DIR} #{remote_user}@#{remote_backup_target}:#{backup_dir}"
+rsync_cmd = "#{RSYNC_APP} #{RSYNC_VERBOSE} #{ssh_port} #{RSYNC_OPTS} #{remote_dir} #{remote_user}@#{remote_backup_target}:#{backup_dir}/current"
 
 logger.info("Started running at: #{Time.now}")
 run_time = Benchmark.realtime do
   begin
-    raise Exception, "Unable to find remote host (#{SSH_SERVER})" unless Ping.pingecho(SSH_SERVER)
+    raise Exception, "Unable to find remote host (#{remote_backup_target})" unless Ping.pingecho(remote_backup_target)
        
     Dir.mkdir("#{EMPTY_DIR}")
-    Open3::popen3("#{rsync_cleanout_cmd}") { |stdin, stdout, stderr|
-      tmp_stdout = stdout.read.strip
-      tmp_stderr = stderr.read.strip
-      logger.info("#{rsync_cleanout_cmd}\n#{tmp_stdout}") unless tmp_stdout.empty?
-      logger.error("#{rsync_cleanout_cmd}\n#{tmp_stderr}") unless tmp_stderr.empty?
-    }
-    Open3::popen3("#{rsync_cmd}") { |stdin, stdout, stderr|
-      tmp_stdout = stdout.read.strip
-      tmp_stderr = stderr.read.strip
-      logger.info("#{rsync_cmd}\n#{tmp_stdout}") unless tmp_stdout.empty?
-      logger.error("#{rsync_cmd}\n#{tmp_stderr}") unless tmp_stderr.empty?
-    }
+    logger.debug(rsync_cleanout_cmd)
+    logger.debug(rsync_cmd)
+#    Open3::popen3("#{rsync_cleanout_cmd}") { |stdin, stdout, stderr|
+#      tmp_stdout = stdout.read.strip
+#      tmp_stderr = stderr.read.strip
+#      logger.info("#{rsync_cleanout_cmd}\n#{tmp_stdout}") unless tmp_stdout.empty?
+#      logger.error("#{rsync_cleanout_cmd}\n#{tmp_stderr}") unless tmp_stderr.empty?
+#    }
+#    Open3::popen3("#{rsync_cmd}") { |stdin, stdout, stderr|
+#      tmp_stdout = stdout.read.strip
+#      tmp_stderr = stderr.read.strip
+#      logger.info("#{rsync_cmd}\n#{tmp_stdout}") unless tmp_stdout.empty?
+#      logger.error("#{rsync_cmd}\n#{tmp_stderr}") unless tmp_stderr.empty?
+#    }
     FileUtils.rmdir("#{EMPTY_DIR}")
   rescue Errno::EACCES, Errno::ENOENT, Errno::ENOTEMPTY, Exception => e
     logger.fatal(e.to_s)
